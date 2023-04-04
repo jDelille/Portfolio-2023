c0adfceac4526ab27d745ca718f9fec3f16562e6
Justin Delille <justind7513@gmail.com>
2023-04-03 23:12:56 -0700

    wip: adding project details modal

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,25 +1,25 @@
-import { RefObject, useEffect, useState } from 'react';
+import gsap from 'gsap';
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { projects } from './projects.json';
 
 import './Projects.scss';
 
-
-
 type ProjectsProps = {
   sectionRef: RefObject<HTMLElement>;
-}
+};
 
 export default function Projects({ sectionRef }: ProjectsProps) {
-
-  const [packageData, setPackageData] = useState<{ downloads?: number } | null>(null);
-
+  const [packageData, setPackageData] = useState<{ downloads?: number } | null>(
+    null
+  );
+  const projectElements = useRef<(HTMLElement | null)[]>([]);
 
   useEffect(() => {
     fetch('https://api.npmjs.org/downloads/point/last-week/jd-react-select')
-      .then(response => response.json())
-      .then(data => setPackageData(data))
-      .catch(error => console.error(error));
-  }, [])
+      .then((response) => response.json())
+      .then((data) => setPackageData(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   const [hoveredProject, setHoveredProject] = useState<number | null>(null);
   const [isAnyProjectHovered, setIsAnyProjectHovered] = useState(false);
@@ -39,34 +39,54 @@ export default function Projects({ sectionRef }: ProjectsProps) {
     // Use sectionElement for calculations or manipulations
   }, [sectionRef]);
 
+  useLayoutEffect(() => {
+    projectElements.current.forEach((project, index) => {
+      gsap.from(project, {
+        duration: 1,
+        opacity: 1,
+        y: 0,
+        stagger: 0.2,
+        delay: index * 0.1,
+        ease: "power4.out", // add an easing function for a smoother animation
+      });
+    });
+  }, []);
 
   return (
-    <section className='projects' id='projects'>
-      {isAnyProjectHovered && (
-        <div className="overlay"></div>
-      )}
+    <section className="projects" id="projects">
+      {isAnyProjectHovered && <div className="overlay"></div>}
       {projects.map((project, i) => (
-        <div className={`project ${hoveredProject === i ? 'hovered' : ''}`} onMouseEnter={() => handleProjectHover(i)} onMouseLeave={handleProjectLeave}>
+        <div
+          className={`project ${hoveredProject === i ? 'hovered' : ''}`}
+          onMouseEnter={() => handleProjectHover(i)}
+          onMouseLeave={handleProjectLeave}
+          ref={(el) => (projectElements.current[i] = el)}
+          key={i}
+        >
           {isAnyProjectHovered && hoveredProject !== i && (
             <div className="overlay"></div>
           )}
-          <div className="project-content" >
-            <p className='project-build'>{project.build}</p>
-            <h1 className='project-name'>{project.name}</h1>
-            <p className='project-description'>{project.desc}</p>
+          <div className="project-content">
+            <p className="project-build">{project.build}</p>
+            <h1 className="project-name">{project.name}</h1>
+            <p className="project-description">{project.desc}</p>
             <div className="project-links">
               {project.npm && (
-                <div className='package-downloads'>
+                <div className="package-downloads">
                   <span>Package Downloads: </span>
                   <p>{packageData && packageData.downloads}</p>
                 </div>
               )}
-              <a href={project.site}>View Site</a>
-              {!project.npm && <a href={project.repo}>Source Code</a>}
             </div>
+
+            {/* <div className="project-links">
+              
+              <a href={project.site} target="_blank">View Site</a>
+              {!project.npm && <a href={project.repo} target="_blank">Source Code</a>}
+            </div> */}
           </div>
         </div>
       ))}
     </section>
-  )
+  );
 }
