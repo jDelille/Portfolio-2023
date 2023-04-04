e2877178cec662f2eb7d06be03ab00243000b4de
Justin Delille <justind7513@gmail.com>
2023-04-03 21:01:49 -0700

    wip: adding contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -10,5 +10,24 @@ export default function Contact({ sectionRef }: ContactProps) {
   const sectionElement = sectionRef.current;
   // Use sectionElement for calculations or manipulations
  }, [sectionRef]);
- return <section className="contact" id="contact"></section>;
+ return <section className="contact" id="contact">
+  <h1>Let's get in touch!</h1>
+  <form action="submit">
+   <div className="input">
+    <label>Your Name</label>
+    <input type="text" placeholder="Your Name" />
+   </div>
+   <div className="input">
+    <label>Your Email</label>
+    <input type="email" placeholder="Your Email" />
+   </div>
+   <div className="input">
+    <label>Your Messaage</label>
+    <textarea placeholder="Message..." />
+   </div>
+   <button type="submit">Send Message</button>
+
+  </form>
+
+ </section>;
 }
