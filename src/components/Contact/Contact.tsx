20565134eeb29f5e67c18688934be3d4c64a6ba8
Justin Delille <justind7513@gmail.com>
2023-04-05 11:58:46 -0700

    fix: fixing styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,59 +4,59 @@ import emailjs from '@emailjs/browser';
 import './Contact.scss';
 
 type ContactProps = {
- sectionRef: RefObject<HTMLElement>;
+  sectionRef: RefObject<HTMLElement>;
 };
 
 export default function Contact({ sectionRef }: ContactProps) {
- const form = useRef<HTMLFormElement>(null);
+  const form = useRef<HTMLFormElement>(null);
 
- const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  emailjs
-   .sendForm(
-    'service_91onskg',
-    'template_j4pd9wj',
-    form.current!,
-    'ckYQsaKiHayd6Sw9N'
-   )
-   .then(
-    (result) => {
-     console.log(result.text);
-    },
-    (error) => {
-     console.log(error.text);
-    }
-   );
- };
+    emailjs
+      .sendForm(
+        'service_91onskg',
+        'template_j4pd9wj',
+        form.current!,
+        'ckYQsaKiHayd6Sw9N'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
- useEffect(() => {
-  const sectionElement = sectionRef.current;
-  // Use sectionElement for calculations or manipulations
- }, [sectionRef]);
+  useEffect(() => {
+    const sectionElement = sectionRef.current;
+    // Use sectionElement for calculations or manipulations
+  }, [sectionRef]);
 
- return (
-  <section className='contact' id='contact'>
-   <h1>Let's get in touch!</h1>
+  return (
+    <section className='contact' id='contact'>
+      <h1>Let's get in touch!</h1>
 
 
-   <form action='submit' ref={form} onSubmit={sendEmail}>
-    <div className='input'>
-     <label>Your Name</label>
-     <input type='text' placeholder='Your Name' name='user_name' />
-    </div>
-    <div className='input'>
-     <label>Your Email</label>
-     <input type='email' placeholder='Your Email' name='user_email' />
-    </div>
-    <div className='input'>
-     <label>Your Messaage</label>
-     <textarea placeholder='Message...' name="message" />
-    </div>
-    <input type="submit" value="Send" className='button' />
+      <form action='submit' ref={form} onSubmit={sendEmail}>
+        <div className='input'>
+          <label>Your Name</label>
+          <input type='text' placeholder='Enter Your Name' name='user_name' required />
+        </div>
+        <div className='input'>
+          <label>Your Email</label>
+          <input type='email' placeholder='Enter Your Email Address' name='user_email' required />
+        </div>
+        <div className='input'>
+          <label>Your Messaage</label>
+          <textarea placeholder="Hi, I'd like to talk to you about an open position at Company X. How soon can you hop on to discuss this?" name="message" required />
+        </div>
+        <input type="submit" value="Send Message" className='button' />
 
-    {/* <button type='submit'>Send Message</button> */}
-   </form>
-  </section>
- );
+        {/* <button type='submit'>Send Message</button> */}
+      </form>
+    </section>
+  );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,123 +7,132 @@ import Scrollspy from 'react-scrollspy';
 import './Hero.scss';
 
 type HeroProps = {
-	onSetActiveLink: (link: string) => void;
-	activeLink: string;
+ onSetActiveLink: (link: string) => void;
+ activeLink: string;
 };
 
 export default function Hero({ onSetActiveLink, activeLink }: HeroProps) {
-	const [scrollEnabled, setScrollEnabled] = useState(true);
+ const [scrollEnabled, setScrollEnabled] = useState(true);
 
-	const sectionIds = ['projects', 'about', 'contact'];
+ const sectionIds = ['projects', 'about', 'contact'];
 
-	useEffect(() => {
-		const handleScroll = () => {
-			if (scrollEnabled) {
-				const currentSection = sectionIds.find((id) => {
-					const section = document.getElementById(id);
-					if (section) {
-						const rect = section.getBoundingClientRect();
-						return (
-							rect.top <= window.innerHeight / 2 &&
-							rect.bottom >= window.innerHeight / 2
-						);
-					}
-					return false;
-				});
-				if (currentSection) {
-					onSetActiveLink(currentSection);
-				}
-			}
-		};
+ useEffect(() => {
+  const handleScroll = () => {
+   if (scrollEnabled) {
+    const currentSection = sectionIds.find((id) => {
+     const section = document.getElementById(id);
+     if (section) {
+      const rect = section.getBoundingClientRect();
+      return (
+       rect.top <= window.innerHeight / 2 &&
+       rect.bottom >= window.innerHeight / 2
+      );
+     }
+     return false;
+    });
+    if (currentSection) {
+     onSetActiveLink(currentSection);
+    }
+   }
+  };
 
-		window.addEventListener('scroll', handleScroll);
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, [sectionIds, onSetActiveLink]);
+  window.addEventListener('scroll', handleScroll);
+  return () => {
+   window.removeEventListener('scroll', handleScroll);
+  };
+ }, [sectionIds, onSetActiveLink]);
 
-	const handleNavClick = (link: string) => {
-		setScrollEnabled(false); // Disable scroll on nav click
-		onSetActiveLink(link);
-		setTimeout(() => setScrollEnabled(true), 1000); // Enable scroll after 1 second delay
-	};
+ const handleNavClick = (link: string) => {
+  setScrollEnabled(false); // Disable scroll on nav click
+  onSetActiveLink(link);
+  setTimeout(() => setScrollEnabled(true), 1000); // Enable scroll after 1 second delay
+ };
 
-	return (
-		<section className='hero'>
-			<h1>Justin Delille</h1>
-			<h2>Front-End Web Developer</h2>
-			<p className='bio'>
-				I like to build interactive websites using React and TypeScript, solve
-				problems with code, and learn about new technologies. View my work,
-				experience, and learn more about who I am. If you like what you see,
-				let's get in touch and make some magic happen.
-			</p>
+ return (
+  <section className='hero'>
+   <h1>Justin Delille</h1>
+   <h2>Front-End Web Developer</h2>
+   <p className='bio'>
+    I like to build interactive websites using React and TypeScript, solve
+    problems with code, and learn about new technologies. View my work,
+    experience, and learn more about who I am. If you like what you see,
+    let's get in touch and make some magic happen.
+   </p>
 
-			<Scrollspy items={sectionIds} currentClassName='is-active'>
-				<nav>
-					<ul>
-						<li
-							className={
-								activeLink === 'projects' ? 'active-nav-link' : 'nav-link'
-							}
-							onClick={() => {
-								onSetActiveLink('projects');
-								handleNavClick('projects');
-							}}>
-							<span className='number'>01</span>
-							<span
-								className={
-									activeLink === 'projects' ? 'extended-line' : 'short-line'
-								}></span>
-							<a href='#projects'>Projects</a>
-						</li>
-						<li
-							className={
-								activeLink === 'about' ? 'active-nav-link' : 'nav-link'
-							}
-							onClick={() => {
-								onSetActiveLink('about');
-								handleNavClick('about');
-							}}>
-							<span className='number'>02</span>
-							<span
-								className={
-									activeLink === 'about' ? 'extended-line' : 'short-line'
-								}></span>{' '}
-							<a href='#about'>About</a>
-						</li>
-						<li
-							className={
-								activeLink === 'contact' ? 'active-nav-link' : 'nav-link'
-							}
-							onClick={() => {
-								onSetActiveLink('contact');
-								handleNavClick('contact');
-							}}>
-							<span className='number'>03</span>
-							<span
-								className={
-									activeLink === 'contact' ? 'extended-line' : 'short-line'
-								}></span>
-							<a href='#contact'>Contact</a>
-						</li>
-					</ul>
-				</nav>
-			</Scrollspy>
+   <Scrollspy items={sectionIds} currentClassName='is-active'>
+    <nav>
+     <ul>
+      <li
+       className={
+        activeLink === 'projects' ? 'active-nav-link' : 'nav-link'
+       }
+       onClick={() => {
+        onSetActiveLink('projects');
+        handleNavClick('projects');
+       }}>
+       <span className='number'>01</span>
 
-			<div className='extra-content'>
-				<img src='../images/me.jpg' alt='' />
-				<div className='linkedin'>
-					<LinkedinIcon />
-					<p>Linkedin</p>
-					<External />
-				</div>
-				<div className='github'>
-					<GithubIcon />
-					<p>Github</p>
-					<External />
-				</div>
-			</div>
-		</section>
-	);
+       <a href='#projects'>
+        <span
+         className={
+          activeLink === 'projects' ? 'extended-line' : 'short-line'
+         }></span>
+        Projects
+       </a>
+      </li>
+      <li
+       className={
+        activeLink === 'about' ? 'active-nav-link' : 'nav-link'
+       }
+       onClick={() => {
+        onSetActiveLink('about');
+        handleNavClick('about');
+       }}>
+       <span className='number'>02</span>
+
+       <a href='#about'>
+        <span
+         className={
+          activeLink === 'about' ? 'extended-line' : 'short-line'
+         }></span>
+        About
+       </a>
+      </li>
+      <li
+       className={
+        activeLink === 'contact' ? 'active-nav-link' : 'nav-link'
+       }
+       onClick={() => {
+        onSetActiveLink('contact');
+        handleNavClick('contact');
+       }}>
+       <span className='number'>03</span>
+
+       <a href='#contact'>
+        <span
+         className={
+          activeLink === 'contact' ? 'extended-line' : 'short-line'
+         }></span>
+        Contact
+       </a>
+      </li>
+     </ul>
+    </nav>
+   </Scrollspy>
+
+   <div className='extra-content'>
+    <img src='../images/me.jpg' alt='' />
+    <div className='linkedin'>
+     <LinkedinIcon />
+     <p>Linkedin</p>
+     <External />
+    </div>
+    <div className='github'>
+     <GithubIcon />
+     <p>Github</p>
+     <External />
+    </div>
+   </div>
+  </section>
+ );
 }
